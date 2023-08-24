24f5a5eba4e854458fa4dbed6d404439ea356466
Rizky Ramadhan <rizkineskar53@gmail.com>
2023-08-24 09:20:36 +0700

    upload seluruh file

---
+++ script.js
@@ -0,0 +1,23 @@
+const callback = function (entries) {
+entries.forEach((entry) => {
+    console.log(entry);
+
+    if (entry.isIntersecting) {
+    entry.target.classList.add("animate-fadeIn");
+    } else {
+    entry.target.classList.remove("animate-fadeIn");
+    }
+});
+};
+
+const observer = new IntersectionObserver(callback);
+
+const targets = document.querySelectorAll(".js-show-on-scroll");
+    targets.forEach(function (target) {
+    target.classList.add("opacity-0");
+    observer.observe(target);
+});
+
+function toCourseDetail(){
+    location.href='course-detail.html';
+}
